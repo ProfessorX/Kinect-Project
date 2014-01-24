f62248a821348e497335d229cf5871f83ece2a6d
Abraham Xiao <q289196573@gmail.com>
2014-01-24 17:36:10 +0400

    Make it simple. Make it concise. For the poster, a maximum of two pages paper is called. And you should find whatever valuable in Mrs. Haleimah's paper, and do-it-yourself.

--- Poster/auto/Poster.el
+++ Poster/auto/Poster.el
@@ -1,7 +1,13 @@
 (TeX-add-style-hook "Poster"
  (lambda ()
+    (LaTeX-add-bibliographies
+     "sigproc")
     (LaTeX-add-labels
-     "sec:introduction")
+     "sec:introduction"
+     "sec:implementation"
+     "sec:results"
+     "sec:related-works"
+     "sec:concl-future-work")
     (TeX-run-style-hooks
      "url"
      "graphicx"
---
+++ Poster/auto/sigproc.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook "sigproc"
+ (lambda ()
+    (LaTeX-add-bibitems
+     "bowman:reasoning"
+     "braams:babel"
+     "clark:pct"
+     "herlihy:methodology"
+     "Lamport:LaTeX"
+     "salas:calculus"
+     "ref:Wang2011")))
+
